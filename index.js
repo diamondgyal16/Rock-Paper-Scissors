95ba38c3d96c20978ae88683ad2b203f647aa5b7
Miyosha Clarke <134561119+diamondgyal16@users.noreply.github.com>
2024-10-03 21:57:41 -0400

    added ui

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+const container = document.querySelector("#container")
+const results = document.querySelector("#results")
+const rock = document.createElement("button")
+rock.textContent = "rock"
+rock.addEventListener("click" , function(){
+    playGame("rock")
+} )
+container.appendChild(rock)
+
+const paper = document.createElement("button")
+paper.textContent = "paper"
+paper.addEventListener("click" , function(){
+    playGame("paper")
+})
+container.appendChild(paper)
+
+const scissors = document.createElement("button")
+scissors.textContent = "scissors"
+scissors.addEventListener("click" , function(){
+    playGame("scissors")
+})
+container.appendChild(scissors)
+
+const h3 = document.createElement("h3")
+results.appendChild(h3)
+h3.textContent = "Results"
+const ul = document.createElement("ul")
+results.appendChild(ul)
+
+
+let round = 1
+let yourScore = 0
+let computerScore = 0
+ 
 function getComputerChoice() {
     const randomNumber = Math.random();
     const choice = Math.floor(randomNumber * 3);
@@ -34,32 +68,51 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function playGame(){
-    let yourScore = 0
-    let computerScore = 0
-    for(let i = 0; i < 5; i++){
-      const result = playRound(prompt("pick your choice, rock, paper, scissors"),getComputerChoice())
-      if(result.includes("You win")){
-        yourScore++
-      }
-      else if(result.includes("You lose")){
-        computerScore++
-      }
-      else{
-        yourScore++
-        computerScore++
-      }
-        alert (result)
-    }
-    if(yourScore > computerScore){
-        alert("You won " + yourScore + "-" + computerScore)
-    }
-    else {
-        alert("You lost " + yourScore + "-" + computerScore)
+function playGame(yourSelection){
+    const computerSelection = getComputerChoice()
+    const result = playRound(yourSelection ,computerSelection)
 
-    }
-   
+      
+    const li = document.createElement("li")
+    ul.appendChild(li)
+    const div = document.createElement("div")
+    div.style.border = "2px solid gray"
+    div.style.display = "flex"
+    div.style.justifyContent = "space-between"
+    div.style.padding = "1em .5em"
+    li.appendChild(div)
+    const roundSpan = document.createElement("span")
+    div.appendChild(roundSpan)
+    const winnerSpan = document.createElement("span")
+    winnerSpan.style.width = "33%"
+    winnerSpan.style.backgroundColor = "red"
+    div.appendChild(winnerSpan)
+    const choiceSpan = document.createElement("span")
+    div.appendChild(choiceSpan)
+    roundSpan.textContent = round 
+
+    if(result.includes("You win")) {
+        yourScore++
+        winnerSpan.textContent = " you "
+        choiceSpan.textContent = yourSelection + " vs " +  computerSelection
+    } else if (result.includes("You lose")) {
+        computerScore++
+        winnerSpan.textContent = " computer "
+        choiceSpan.textContent = computerSelection + " vs " +  yourSelection
+    } else {
+        yourScore++
+        computerScore++
+        winnerSpan.textContent = " tie "
+        choiceSpan.textContent = yourSelection
     }
 
-        
-playGame()
+    if(round === 5) {
+        if(yourScore > computerScore){
+            alert("You won " + yourScore + "-" + computerScore)
+        } else {
+            alert("You lost " + yourScore + "-" + computerScore)
+        }
+    } 
+
+    round++
+}
